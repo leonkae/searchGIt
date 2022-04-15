ad03e2ab741cb1ef890ed0814fa9d1c85182c36e
leonkae <leon29kae@gmail.com>
2022-04-15 11:38:18 +0300

    modified components

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onKeyDownEVent(event:any){
+    this.router.navigate(['search-result'], {relativeTo:this.route});
+  }
+  
+  sendData(event: any) {
+    console.log(event.target.value);
   }
 
+  // constructor() { }
+
+  ngOnInit(): void {}
 }
